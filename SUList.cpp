e08611e82a01625cdf42743085fcaa4ff0915a68
Billy <wromansky@protonmail.com>
2018-04-21 17:17:08 -0400

    Define functions

--- SUList.cpp
+++ SUList.cpp
@@ -6,43 +6,91 @@ using namespace std;
 template <class DataType>
 SUList<DataType>::SUList()
 {
-    //
-    //
+    // List starts with nothing
+    head = nullptr;
+    tail = nullptr;
 }
 
 template <class DataType>
-SUList<DataType>::SUList(const SUList&)
+SUList<DataType>::SUList(const SUList& old)
 {
-    //
-    //
+    this->head = nullptr;
+    ListNode* oldCursor = old.head;
+    while(oldCursor != nullptr)
+    {
+        this->putfront(oldCursor->data);
+        oldCursor = oldCursor->next;
+    }
+    
 }
 
 template <class DataType>
 SUList<DataType>::~SUList()
 {
-    //
-    //
+    ListNode* cursor = head;
+    ListNode* temp;
+
+    while(cursor)
+    {
+        temp = cursor -> next;
+        delete cursor;
+        cursor = temp;
+    }
+
 }
 
 template <class DataType>
 DataType SUList<DataType>::getFront()
 {
-    //
-    //
+    if(head == nullptr)
+    {
+        throw "List is empty...\n";
+    }
+
+    DataType front = head->data;
+    ListNode* temp = head;
+    head = head->next;
+    delete temp;
+
+    return front;
 }
 
 template <class DataType>
 DataType SUList<DataType>::getBack()
 {
-    //
+   if(head == nullptr)
+   {
+       throw "List is empty...\n";
+   }
+
+   DataType back = tail->data;
+   ListNode* temp = tail;
+   tail = tail->prev;
+   delete temp;
+
+   return back;
+}
+
+template <class DataType>
+void SUList<DataType>::putFront(const DataType& n)
+{
+    
     //
 }
 
 template <class DataType>
-void SUList<DataType>::putFront(const DataType&)
+void SUList<DataType>::putBack(const DataType& n)
 {
-    //
-    //
+    ListNode* newNode = new ListNode;
+    newNode->data = n;
+    newNode->next = nullptr;
+    newNode->prev = nullptr;
+
+    if(head == nullptr)
+    {
+        head = tail = newNode;
+        return;
+    }
 }
 
 template <class DataType>
