51148928d7d739c0a8c39a111d2e6999c3081bc0
Billy <wromansky@protonmail.com>
2018-04-21 14:40:10 -0400

    modified:   SUList.cpp

---
+++ SUList.cpp
@@ -0,0 +1,20 @@
+#include "SUList.h"
+#include <iostream>
+
+using namespace std;
+
+template <class DataType>
+SUList<DataType>::SUList()
+{
+    //
+    //
+
+}
+
+template <class DataType>
+SUList<DataType>::SUList(const SUList&)
+{
+    //
+    //
+}
+
