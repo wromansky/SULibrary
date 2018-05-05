16736e2e161c7f2d26980cd964e26d1a8904124b
Billy <wromansky@protonmail.com>
2018-05-05 02:42:38 -0400

    another drunk commit

--- src/SUQueue.cpp
+++ src/SUQueue.cpp
@@ -1,5 +1,36 @@
 template <class DataType>
+int SUQueueList<DataType>::size()
+{
+    return list.size();
+}
+
+template <class DataType>
+bool SUQueueList<DataType>::isEmpty()
+{
+    //
+    return false;
+}
+
+template <class DataType>
 void SUQueueList<DataType>::enqueue(const DataType& n)
 {
     list.putBack(n);
+}
+
+template <class DataType>
+void SUQueueList<DataType>::dequeue(DataType& n)
+{
+    list.getFront();
+}
+
+template <class DataType>
+void SUQueueList<DataType>::printQueue()
+{
+    //
+}
+
+template <class DataType>
+SUQueueList<DataType>&::operator=(constSUQueueList<DataType>&)
+{
+    //
 }
